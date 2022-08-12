43adb335ff9eef946d73eb0a2e2406e74fbf079d
unknown <broken.wayan@gmail.com>
2022-08-12 13:50:12 +0800

    add body care page

---
+++ assets/js/script2.js
@@ -0,0 +1,79 @@
+const { createApp, ref, computed, onMounted, onUnmounted, reactive } = Vue;
+
+const App = createApp({
+  setup() {
+    const version = ref(0);
+    const data = reactive({
+      region: '',
+      resellers: []
+    });
+
+    const width = ref(0);
+    const lgScreen = 1024;
+    let resellers = ref([]);
+    let regions = ref([]);
+
+    version.value = 1;
+    width.value = document.documentElement.clientWidth;
+
+    const isMobile = computed(() => {
+      let mobile = false;
+      if(width.value < lgScreen) {
+        mobile = true;
+      }
+      return mobile;
+    });
+
+    function getDimensions() {
+      width.value = document.documentElement.clientWidth;
+    }
+
+    const selectRegion = () => {
+      data.resellers = resellers.value.filter(item => item.region === data.region);
+    }
+
+    function readTextFile(file, callback) {
+      var rawFile = new XMLHttpRequest();
+      rawFile.overrideMimeType("application/json");
+      rawFile.open("GET", file, true);
+      rawFile.onreadystatechange = function() {
+          if (rawFile.readyState === 4 && rawFile.status == "200") {
+              callback(rawFile.responseText);
+          }
+      }
+      rawFile.send(null);
+    }
+
+    readTextFile("../assets/json/region.json", function(text){
+      var data = JSON.parse(text); //parse JSON
+      regions.value =  data;
+    });
+
+    readTextFile("../assets/json/reseller.json", function(text){
+      var data = JSON.parse(text); //parse JSON
+      resellers.value =  data;
+      selectRegion();
+    });
+ 
+    onMounted(() => {
+      window.addEventListener('resize', getDimensions);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener('resize', getDimensions);
+    });
+
+    return {
+      version,
+      width,
+      getDimensions,
+      selectRegion,
+      readTextFile,
+      isMobile,
+      resellers,
+      regions,
+      data
+    }
+  }
+});
+App.mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html', 'body-care/index.html'],
   theme: {
     extend: {
       container: {
